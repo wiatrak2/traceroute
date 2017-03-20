88993fa9375f17d697d4d10ef15a5e3914df1472
Wojtek Pratkowiecki <wpratkowiecki@gmail.com>
2017-03-20 23:07:51 +0100

    create files

---
+++ receive.h
@@ -0,0 +1,11 @@
+##ifndef receive_h
+#define receive_h 
+
+#include "utils.h"
+
+class Receive
+{
+	int socketfd;
+}
+
+#endif /* receive_h */
---
+++ utils.h
@@ -0,0 +1,3 @@
+#include <cstdint>
+
+u_int16_t compute_icmp_checksum (const void *buff, int length);
